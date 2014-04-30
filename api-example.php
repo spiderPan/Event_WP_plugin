5fdba18882438f480ad8ae0f4155a6749c1980c4
spiderPan <panbanglanfeng@gmail.com>
2014-04-30 12:47:50 -0400

    wp set up

---
+++ api-example.php
@@ -0,0 +1,89 @@
+<?php 
+	include 'MailChimp.php';
+	
+	$MailChimp = new \Drewm\MailChimp('e0504477c7e6147afb13aad8a5ffeebf-us8');
+	## Add Segment
+	$group_id = 8821;
+	$group_name = 'new_group_added_by_api';
+	$add_segment = $MailChimp -> call('lists/segment-add', array(
+				'id'                => '2f5a809940',
+				'opts'=>array(
+					'type'=>'saved',
+					'name'=> 'test_seg_get_from_group',
+					'segment_opts'=>array(
+						'match'=>'all',
+						'conditions'=>array(
+							array(
+								'field'=>'interests-'.$group_id,
+								'op'=>'all',
+								'value'=>$group_name
+							)
+							
+						)
+					)
+				)
+		));
+	echo var_dump($add_segment);
+		/**/
+		
+	## Add interest-grouping
+	
+	/*
+	$new_group_name = 'new_group_added_by_api';
+	$new_group = $MailChimp->call('lists/interest-grouping-add', array(
+					'id'                => '2f5a809940',
+					'name'=>$new_group_name,
+					'type'=>'checkboxes',
+					'groups'=>array(
+						'name'=>$new_group_name
+					)
+			));
+	$new_group_id = $new_group['id'];
+	echo $new_group_id;
+	*/
+	## Add subscribe
+	/*
+	$event_id = 8821;
+	$add_subscribe = $MailChimp->call('lists/subscribe', array(
+					'id'                => '2f5a809940',
+					'email'             => array('email'=>'pan@testinspi.com'),
+					'merge_vars'        => array('groupings'=>array(
+						'id'=>$event_id,
+						'groups'=>array('event_1')
+					)),
+					'double_optin'      => false,
+					'update_existing'   => true,
+					'replace_interests' => false,
+					'send_welcome'      => false,
+			));
+	*/
+	#echo var_dump($add_subscribe);
+	?>
+
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>MailChimp Test</title>
+	</head>
+	
+	<body>
+	<?php 
+		
+		/*if($retrived_emails){
+			echo '<ul>';
+			foreach($retrived_emails as $email){
+				echo '<li>'.$email['email'].'</li>';
+			}
+			echo '</ul>';
+		}*/
+	?>
+		<form method = "post">
+			Email<input type="email" name="email"><br>
+			First Name<input type="text" name = "fname"><br>
+			Last Name<input type="text" name = "lname"><br>
+			Event<input type="text" name = "event"><br>
+			<input type="submit" value="submit">
+		</form>		
+	</body>
+</html>
--- mailchimp.plugin.php
+++ mailchimp.plugin.php
@@ -7,3 +7,135 @@ Author: Inspiratica
 Version: 1.0
 Author URI: http://inspiratica.ca/
 */
+
+/************* Post Type inspi_event ********************/
+add_action('init', 'mailchimp_event_init');
+
+function mailchimp_event_init() {
+	register_post_type(
+		'inspi_event',
+		array(
+			'labels' => array(
+				'name'          => 'Events',
+				'singular_name' => 'Event'
+			),
+			'public' => true
+		)
+	);
+
+	wp_enqueue_script('jquery-time', plugins_url('js/jquery.timepicker.min.js', __FILE__), array('jquery'));
+	wp_enqueue_script('jquery-date', plugins_url('js/jquery-ui-1.10.4.custom.min.js', __FILE__), array('jquery'));
+	wp_enqueue_style('jquery-date', plugins_url('css/jquery-ui-1.10.4.custom.min.css', __FILE__));
+	wp_enqueue_style('jquery-time', plugins_url('css/jquery.timepicker.css', __FILE__));
+}
+
+/************* Metabox inspi_event ********************/
+function get_mailchimp_fields() {
+	return array(
+		'event'         => array(
+			'name'     => 'Event',
+			'type'     => 'text',
+			'required' => true
+		),
+		'event_date'    => array(
+			'name'     => 'Event Date',
+			'type'     => 'date',
+			'required' => true
+		),
+		'event_time'    => array(
+			'name'     => 'Event Time',
+			'type'     => 'time',
+			'required' => true
+		),
+		'email_date'    => array(
+			'name'     => 'Email Date',
+			'type'     => 'date',
+			'required' => true
+		),
+		'email_time'    => array(
+			'name'     => 'Email Time',
+			'type'     => 'time',
+			'required' => true
+		),
+		'email_content' => array(
+			'name'     => 'Email Content',
+			'type'     => 'textarea',
+			'required' => true
+		)
+	);
+}
+
+add_action('admin_init', 'mailchimp_metabox');
+
+function mailchimp_metabox() {
+	add_meta_box('mailchimp_meta', 'Event Information', 'mailchimp_metabox_callback', 'inspi_event', 'normal');
+}
+
+function mailchimp_metabox_callback($post) {
+	wp_nonce_field('inspi_mailchimp_event_meta_box', 'inspi_mailchimp_event_meta_box_nonce');
+
+	$mailchimp_fields = get_mailchimp_fields();
+	?>
+	<dl>
+		<?php
+		foreach ($mailchimp_fields as $key => $info) :?>
+			<dt><label for="inspi-<?php echo $key; ?>"><?php echo $info['name']; ?></label></dt>
+			<?php $value = get_post_meta($post->ID, 'inspi-' . $key, true); ?>
+			<dd>
+				<?php switch ($info['type']):
+				case 'textarea':
+					?>
+					<textarea id="inspi-<?php echo $key; ?>" name="inspi-<?php echo $key; ?>" rows="5" cols="100" <?php echo $info['required'] ? 'required="required"' : ''; ?>><?php echo $value; ?></textarea>
+				<?php break;
+				case 'text':
+				?>
+				<input type="text" id="inspi-<?php echo $key; ?>" name="inspi-<?php echo $key; ?>" value="<?php echo $value; ?>" <?php echo $info['required'] ? 'required="required"' : ''; ?> />
+				<?php break;
+				case 'date':
+				?>
+				<input type="text" id="inspi-<?php echo $key; ?>" name="inspi-<?php echo $key; ?>" value="<?php echo $value; ?>" <?php echo $info['required'] ? 'required="required"' : ''; ?> />
+					<script>jQuery("#inspi-<?php echo $key; ?>").datepicker();</script>
+				<?php break;
+				case 'time':
+				?>
+				<input type="text" id="inspi-<?php echo $key; ?>" name="inspi-<?php echo $key; ?>" value="<?php echo $value; ?>" <?php echo $info['required'] ? 'required="required"' : ''; ?> />
+					<script>jQuery("#inspi-<?php echo $key; ?>").timepicker();</script>
+					<?php break;
+
+				endswitch;?>
+
+			</dd>
+		<?php endforeach; ?>
+	</dl>
+<?php
+
+}
+
+function inspi_mailchimp_event_plugin_save_postdata($post_id) {
+	if (!isset($_POST['inspi_mailchimp_event_meta_box_nonce']) || !wp_verify_nonce($_POST['inspi_mailchimp_event_meta_box_nonce'], plugin_basename(__FILE__))) {
+		return $post_id;
+	}
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return $post_id;
+	}
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $post_id)) {
+			return $post_id;
+		}
+	} else {
+		if (!current_user_can('edit_post', $post_id)) {
+			return $post_id;
+		}
+	}
+
+	$fields = get_mailchimp_fields();
+	foreach ($fields as $fieldId => $info) {
+
+		$value = $_POST['inspi-' . $fieldId];
+		update_post_meta($post_id, 'inspi-' . $fieldId, $value);
+	}
+
+	return "";
+}
+
+add_action('save_post', 'inspi_mailchimp_event_plugin_save_postdata');
